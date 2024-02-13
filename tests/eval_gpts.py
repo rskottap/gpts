439a50567c5a8cf291587e3a652ebe91deb1161d
Ramya Kottapalli <ramya.kottapalli@aicadium.ai>
2024-02-13 21:31:52 +0000

    Fix negative int/float outputs. Adding QA template to prompt instead of ending it in a '?'

--- tests/eval_gpts.py
+++ tests/eval_gpts.py
@@ -3,78 +3,79 @@
 import gpts
 
 cpu_only = True
+template = """Q: {question}\nA: """
 
 def ask_mistral_ounces_int():
     question = "How many ounces are in a pound?"
-    return (gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=int), 16)
+    return (gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int), 16)
 
 def ask_mistral_ounces_float():
     question = "How many ounces are in a pound?"
-    return (gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=float), 16.0)
+    return (gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float), 16.0)
 
 def ask_mistral_days_int():
     question = "How many days are in a week?"
-    return (gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=int), 7)
+    return (gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int), 7)
 
 def ask_mistral_days_float():
     question = "How many days are in a week?"
-    return (gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=float), 7.0)
+    return (gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float), 7.0)
 
 def ask_mistral_dozen_int():
     question = "How many eggs are in a dozen?"
-    return (gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=int), 12)
+    return (gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int), 12)
 
 def ask_mistral_dozen_float():
     question = "How many eggs are in a dozen?"
-    return (gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=float), 12.0)
+    return (gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float), 12.0)
 
 def ask_mixtral_ounces_int():
     question = "How many ounces are in a pound?"
-    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=int), 16)
+    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int), 16)
 
 def ask_mixtral_ounces_float():
     question = "How many ounces are in a pound?"
-    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=float), 16.0)
+    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float), 16.0)
 
 def ask_mixtral_days_int():
     question = "How many days are in a week?"
-    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=int), 7)
+    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int), 7)
 
 def ask_mixtral_days_float():
     question = "How many days are in a week?"
-    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=float), 7.0)
+    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float), 7.0)
 
 def ask_mixtral_dozen_int():
     question = "How many eggs are in a dozen?"
-    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=int), 12)
+    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int), 12)
 
 def ask_mixtral_dozen_float():
     question = "How many eggs are in a dozen?"
-    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=float), 12.0)
+    return (gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float), 12.0)
 
 def ask_openorca_ounces_int():
     question = "How many ounces are in a pound?"
-    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=int), 16)
+    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=int), 16)
 
 def ask_openorca_ounces_float():
     question = "How many ounces are in a pound?"
-    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=float), 16.0)
+    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=float), 16.0)
 
 def ask_openorca_days_int():
     question = "How many days are in a week?"
-    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=int), 7)
+    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=int), 7)
 
 def ask_openorca_days_float():
     question = "How many days are in a week?"
-    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=float), 7.0)
+    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=float), 7.0)
 
 def ask_openorca_dozen_int():
     question = "How many eggs are in a dozen?"
-    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=int), 12)
+    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=int), 12)
 
 def ask_openorca_dozen_float():
     question = "How many eggs are in a dozen?"
-    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=float), 12.0)
+    return (gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=float), 12.0)
 
 if __name__ == '__main__':
     funcs = [v for k,v in globals().items() if k.startswith('ask')]
--- tests/test_gpts_fine.py
+++ tests/test_gpts_fine.py
@@ -3,74 +3,76 @@
 import gpts
 
 cpu_only = True
+template = """Q: {question}\nA: """
+
 def test_mistral_ounces_int():
     question = "How many ounces are in a pound?"
-    assert gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=int) == 16
+    assert gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int) == 16
 
 def test_mistral_ounces_float():
     question = "How many ounces are in a pound?"
-    assert gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=float) == 16.0
+    assert gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float) == 16.0
 
 def test_mistral_days_int():
     question = "How many days are in a week?"
-    assert gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=int) == 7
+    assert gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int) == 7
 
 def test_mistral_days_float():
     question = "How many days are in a week?"
-    assert gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=float) == 7.0
+    assert gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float) == 7.0
 
 def test_mistral_dozen_int():
     question = "How many eggs are in a dozen?"
-    assert gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=int) == 12
+    assert gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int) == 12
 
 def test_mistral_dozen_float():
     question = "How many eggs are in a dozen?"
-    assert gpts.Mistral(cpu_only=cpu_only).ask_for(question, type=float) == 12.0
+    assert gpts.Mistral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float) == 12.0
 
 def test_mixtral_ounces_int():
     question = "How many ounces are in a pound?"
-    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=int) == 16
+    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int) == 16
 
 def test_mixtral_ounces_float():
     question = "How many ounces are in a pound?"
-    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=float) == 16.0
+    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float) == 16.0
 
 def test_mixtral_days_int():
     question = "How many days are in a week?"
-    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=int) == 7
+    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int) == 7
 
 def test_mixtral_days_float():
     question = "How many days are in a week?"
-    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=float) == 7.0
+    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float) == 7.0
 
 def test_mixtral_dozen_int():
     question = "How many eggs are in a dozen?"
-    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=int) == 12
+    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=int) == 12
 
 def test_mixtral_dozen_float():
     question = "How many eggs are in a dozen?"
-    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(question, type=float) == 12.0
+    assert gpts.Mixtral(cpu_only=cpu_only).ask_for(template.format(question=question), type=float) == 12.0
 
 def test_openorca_ounces_int():
     question = "How many ounces are in a pound?"
-    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=int) == 16
+    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=int) == 16
 
 def test_openorca_ounces_float():
     question = "How many ounces are in a pound?"
-    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=float) == 16.0
+    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=float) == 16.0
 
 def test_openorca_days_int():
     question = "How many days are in a week?"
-    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=int) == 7
+    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=int) == 7
 
 def test_openorca_days_float():
     question = "How many days are in a week?"
-    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=float) == 7.0
+    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=float) == 7.0
 
 def test_openorca_dozen_int():
     question = "How many eggs are in a dozen?"
-    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=int) == 12
+    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=int) == 12
 
 def test_openorca_dozen_float():
     question = "How many eggs are in a dozen?"
-    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(question, type=float) == 12.0
+    assert gpts.OpenOrca(cpu_only=cpu_only).ask_for(template.format(question=question), type=float) == 12.0
